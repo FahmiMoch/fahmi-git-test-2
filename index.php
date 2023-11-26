e361357de79e9c2decd89ede6aad0799e1d7cfc6
FahmiMoch <fahmifadillah18@gmail.com>
2023-11-26 21:48:50 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Git Remote</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quas facilis ipsa ab atque laborum vero accusamus facere debitis molestiae eaque dolorem reiciendis, porro molestias sequi, inventore possimus in sunt!</p>
 </body>
 </html>
